abe112aac5bc530307fa04f8b36316f6338a67d3
mercury-wandering <2084174661@qq.com>
2024-12-03 23:01:06 +0800

    第一次提交代码

---
+++ health_backed/src/main/java/com/igeek/health/App.java
@@ -0,0 +1,9 @@
+package com.igeek.health;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ health_service/src/main/java/com/igeek/health/App.java
@@ -0,0 +1,13 @@
+package com.igeek.health;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
